431b70296c0a2cbb8dc658c5d297f96e915b2cd9
breckmcc$ <breckmcc@gmail.com>
2015-09-15 09:36:35 -0400

    practice

---
+++ intro to r class.R
@@ -0,0 +1,33 @@
+ # CALCULATOR
+ 2 + 2
+
+ # ORDER OF OPERATIONS
+ 3 + 4 * (10 - 5)^2
+
+ # SCIENTIFIC NOTATION
+  0.0003
+  3* 10^(-4)
+  3e-4
+  
+  #math functions
+  log(2) # natural log
+  log10(2) # base-10 log
+  
+  exp(0.5) # e^(1/2)
+  
+  # comparisons
+  # operator
+  
+ # equal to
+  2==3
+  2==2
+  
+  # no equal to
+  2==3
+  2==2
+  
+  
+  # greater/less than
+  2
+  
+  
